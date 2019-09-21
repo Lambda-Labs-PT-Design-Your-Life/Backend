a9ad39fbb6b08ae8a63e4f6712fbbdb91634fff2
Cody Brewer <brewer.cody.m@gmail.com>
2019-09-20 19:15:20 -0600

    removed cross-env

--- api/server.test.js
+++ api/server.test.js
@@ -6,5 +6,9 @@ describe('The Server', () => {
     it('should run the testing environment', () => {
       expect(process.env.DB_ENV).toBe('testing');
     });
+    it('should return a 200 on the index route', async () => {
+      const res = await request(server).get('/');
+      expect(res.status).toBe(200);
+    })
   });
 });
