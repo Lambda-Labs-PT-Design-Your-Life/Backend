7b207b7ab8c107c827d4afac674ed4826312f83c
CodyBrewer <brewer.cody.m@gmail.com>
2019-09-22 15:47:54 -0600

    knex set up

---
+++ auth/auth-router.test.js
@@ -0,0 +1,21 @@
+const request = require('supertest');
+const jwt = require('jsonwebtoken');
+const server = require('../api/server');
+const db = require('../database/dbConfig');
+
+beforeEach(async () => {
+  await db.from('users').truncate();
+});
+
+describe('POST /register', () => {
+  it('should return a 400 when missing username', async () => {
+    const missingUsername = {
+      password: 'password123',
+      email: 'email@email.com'
+    };
+
+    const missingUsernameRes = await request(server)
+      .post('/api/auth/register')
+      .send(missingUsername);
+  });
+});
