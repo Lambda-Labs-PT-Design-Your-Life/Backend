e40f0dd63e142e10eb2526043bd4c91ad5c22625
CodyBrewer <brewer.cody.m@gmail.com>
2019-09-22 16:41:26 -0600

    typo fix

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -4,7 +4,7 @@ module.exports = authRouter;
 
 authRouter.post('/register', (req, res) => {
   let user = req.body;
-  !user.username || !user.email || !user.passwordf
+  !user.username || !user.email || !user.password
     ? res.status(400).json({ message: 'Please include us' })
     : res.status(200).json({ messatge: 'Post Successful' });
 });
