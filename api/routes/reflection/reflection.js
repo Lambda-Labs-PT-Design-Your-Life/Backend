7e987d0675e9b7b58081c4205ea0c28cd01b32b1
CodyBrewer <brewer.cody.m@gmail.com>
2019-09-24 16:24:56 -0600

    reflection routes created

--- api/routes/reflection/reflection.js
+++ api/routes/reflection/reflection.js
@@ -40,3 +40,70 @@ reflectionRouter.get(':/reflectionId', (req, res) => {
       res.status(500).json({ message: err.message });
     });
 });
+
+reflectionRouter.get('/user/:userId', (req, res) => {
+  const { userId } = req.params;
+
+  Reflection.findAllReflectionsByUser(userId)
+    .then(foundReflections => {
+      if (!foundReflections) {
+        res
+          .status(404)
+          .json({ message: 'No reflections created by that user' });
+      } else {
+        res.status(200).json(foundReflections);
+      }
+    })
+    .catch(err => {
+      res.status(500).json({ message: err.message });
+    });
+});
+
+reflectionRouter.delete(':/reflectionId', (req, res) => {
+  const { reflectionId } = req.params;
+
+  Reflection.deleteReflection(reflectionId)
+    .then(deletedReflection => {
+      if (!deletedReflection) {
+        res.status(404).json({ message: 'No reflection by the reflectionId' });
+      } else {
+        res.status(200).json({ message: 'reflection deleted' });
+      }
+    })
+    .catch(err => {
+      res.status(500).json({ message: err.message });
+    });
+});
+
+reflectionRouter.put('/:reflectionId', (req, res) => {
+  const { reflectionId } = req.params;
+  const { reflection } = req.body;
+
+  Reflection.updateReflection(reflectionId, reflection)
+    .then(updatedReflection => {
+      if (!updatedReflection) {
+        res
+          .status(404)
+          .json({ message: 'No reflection found by that reflectionId' });
+      } else {
+        res.status(200).json({ message: 'reflection updated' });
+      }
+    })
+    .catch(err => {
+      res.status(500).json({ message: err.message });
+    });
+});
+
+reflectionRouter.get('/reflections', (req, res) => {
+  Reflection.finddAllReflections()
+    .then(reflections => {
+      if (!reflections) {
+        res.status(404).json({ message: 'No reflections' });
+      } else {
+        res.status(200).json(reflections);
+      }
+    })
+    .catch(err => {
+      res.status(500).json({ message: err.message });
+    });
+});
