01e2c44cec2dd4c67a8f172a86c39c50618e2043
Cody Brewer <brewer.cody.m@gmail.com>
2019-09-25 13:43:37 -0600

    had to change req.headers.Authorization to lc

--- middleware/authenticate.js
+++ middleware/authenticate.js
@@ -1,7 +1,7 @@
 const jwt = require('jsonwebtoken');
 
 module.exports = (req, res, next) => {
-  const token = req.headers.Authorization;
+  const token = req.headers.authorization;
 
   if (token) {
     jwt.verify(token, process.env.JWT_SECRET, (err, decodedToken) => {
